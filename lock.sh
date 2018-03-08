e2a9cb99afe9a710b6fdd076779d747d0c5758bb
user.name <goosewood@protonmail.com>
2018-03-08 17:38:31 -0500

    made it crop out the 'scrot taken' text

--- lock.sh
+++ lock.sh
@@ -6,10 +6,13 @@
 killall compton
 
 # makes the neofetch, and scrots it (delay there for proccesing, change it to your pc's speed)
-urxvt +sb -name lock -e neofetch -source ~/Images/Fem/qt.jpg --scrot_cmd 'scrot -z -u -d 0 /tmp/lock.png' -s & sleep 1.7; kill $!
+urxvt +sb -name lock -e neofetch --song_shorthand on --source ~/Images/Fem/qt.jpg --scrot_cmd 'scrot -z -u -d 0 /tmp/lock.png' -s & sleep 1.7; kill $!
 
 # turns compton back on, and waits a sec (again, just giving it a sec to load)
 compton -b & sleep .3
+
+# crops image, this is hardcoded since I didn't want to deal with %'s, so change based on your screen size
+convert /tmp/lock.png -crop 490x245+0+0 /tmp/lock.png
 
 # Define image locations
 ICON=/tmp/lock.png
